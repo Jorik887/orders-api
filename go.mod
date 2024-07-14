33e144f7733e79a1736a04254fbcfcf048de3fe6
Jorik887 <jopik1242@gmail.com>
2024-07-14 19:33:44 +0900

    chi router and middleware added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Jorik887/orders-api
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -18,5 +25,6 @@ func main() {
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
+
 	w.Write([]byte("Hello, world"))
 }
